679d83cfc224bcb0056611d6024b048b972a0e8a
Oliver Philbin-Briscoe <ophilbinbriscoe.ld@gmail.com>
2017-03-27 21:04:22 -0400

    initial commit

---
+++ Source/Behaviours/Tweener.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToBoldlyPlay.Tweening
+{
+	public class Tweener : MonoBehaviour
+	{
+		private void Awake ()
+		{
+
+		}
+
+		private void Start ()
+		{
+
+		}
+
+		private void Update ()
+		{
+
+		}
+	}
+}
