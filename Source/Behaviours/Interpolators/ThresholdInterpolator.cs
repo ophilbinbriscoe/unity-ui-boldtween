28f173cb6b5a6649187d1368a91da729c55ef315
Oliver Philbin-Briscoe <ophilbinbriscoe.ld@gmail.com>
2017-04-02 01:01:34 -0400

    threshold callbacks

---
+++ Source/Behaviours/Interpolators/ThresholdInterpolator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+namespace ToBoldlyPlay.Tweening
+{
+	public class ThresholdInterpolator : Interpolator
+	{
+		[Serializable]
+		public class ThresholdEvent : UnityEvent<bool> { }
+
+		[SerializeField]
+		[Range( 0.0f, 1.0f )]
+		private float threshold;
+
+		public float Threshold
+		{
+			get
+			{
+				return threshold;
+			}
+
+			set
+			{
+				threshold = Mathf.Clamp01( value );
+			}
+		}
+
+		public UnityEvent onTrue, onFalse;
+		public ThresholdEvent onCross;
+
+		private bool init;
+		private bool current;
+
+		public override void Interpolate ( float t )
+		{
+			bool value = threshold == 0.0f ? t > threshold : t >= threshold;
+
+			if ( current != value || !init )
+			{
+				init = true;
+
+				onCross.Invoke( current = value );
+
+				if ( current )
+				{
+					onTrue.Invoke();
+				}
+				else
+				{
+					onFalse.Invoke();
+				}
+			}
+		}
+	}
+}
