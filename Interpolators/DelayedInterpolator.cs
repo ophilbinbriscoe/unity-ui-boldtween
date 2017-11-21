3e4d592766ce37e78524974e0f359c6cd6fe1a28
Oliver Philbin-Briscoe <ophilbinbriscoe.ld@gmail.com>
2017-11-20 21:38:17 -0500

    some new interpolators

---
+++ Interpolators/DelayedInterpolator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System;
+
+namespace BoldTween
+{
+	public class DelayedInterpolator : Interpolator
+	{
+		[SerializeField]
+		private float delay;
+
+		[SerializeField]
+		private bool useUnscaledTime;
+
+		private Queue<Vector2> buffer;
+
+		[SerializeField]
+		private TweenEvent onDelayedInterpolate;
+
+		protected override void OnInterpolate ( float t )
+		{
+			buffer.Enqueue( new Vector2( useUnscaledTime ? Time.unscaledTime : Time.time, t ) );
+		}
+
+		private void Update ()
+		{
+			while ( buffer.Count > 0 )
+			{
+				while ( buffer.Peek().x + delay < (useUnscaledTime ? Time.unscaledTime : Time.time) )
+				{
+					onDelayedInterpolate.Invoke( buffer.Dequeue().y );
+				}
+			}
+		}
+	}
+}
---
+++ Interpolators/UIPositionInterpolator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BoldTween.UI
+{
+	public class UIPositionInterpolator : RectTransformInterpolator<Vector2>
+	{
+		protected override void Reset ()
+		{
+			base.Reset();
+
+			a = target.anchoredPosition;
+			b = target.anchoredPosition;
+		}
+
+		protected override void OnInterpolate ( RectTransform target, Vector2 value )
+		{
+			tracker.Clear();
+			tracker.Add( this, target, DrivenTransformProperties.AnchoredPositionX | DrivenTransformProperties.AnchoredPositionY );
+
+			target.anchoredPosition = value;
+		}
+
+		protected override Vector2 Interpolate ( Vector2 a, Vector2 b, float t )
+		{
+			return a + (b - a) * t;
+		}
+	}
+}
